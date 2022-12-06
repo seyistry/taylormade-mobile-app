fa4a4d8717a2f685003a1173a08f59ca819874fe
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-12-06 23:33:43 +0100

    add statusbar

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import Constants from "expo-constants";
+import { blackBg } from "./utils/color";
+
+function AppStatusBar({ backgroundColor, ...props }) {
+    return (
+        <SafeAreaView
+            style={{ backgroundColor, height: Constants.statusBarHeight }}
+        >
+            <StatusBar
+                translucent
+                backgroundColor={backgroundColor}
+                {...props}
+            />
+        </SafeAreaView>
+    );
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <>
+            <AppStatusBar backgroundColor={blackBg} style="light" />
+            <View style={styles.container}>
+                <Text>Open up App.js to start working on your app!</Text>
+            </View>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
