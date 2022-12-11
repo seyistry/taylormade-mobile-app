0cfe9a35ab27386a6a1d0ba54dcc68278f2b6cee
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-12-11 21:02:23 +0100

    add new step

---
+++ frontend/screens/onboarding/StepTwo.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function StepTwo() {
+  return (
+	<View>
+	  <Text>StepTwo</Text>
+	</View>
+  )
+}
+
+const styles = StyleSheet.create({})
