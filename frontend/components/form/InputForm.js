f80666bb1df8454664f8689d0871d4dbc5c6b730
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-12-11 18:47:38 +0100

    update font

--- frontend/components/form/InputForm.js
+++ frontend/components/form/InputForm.js
@@ -86,7 +86,7 @@ const styles = StyleSheet.create({
     },
     input: {
         color: greyText,
-        fontFamily: "LatoM",
+        fontFamily: "LatoR",
         width: "84%",
         // paddingHorizontal: "5%",
     },
