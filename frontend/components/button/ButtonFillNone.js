01a42ebab18f1acac1a9921d35e87121b49a8a68
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-12-12 19:14:31 +0100

    update props

--- frontend/components/button/ButtonFillNone.js
+++ frontend/components/button/ButtonFillNone.js
@@ -2,9 +2,9 @@ import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
 import { white } from "../../utils/color";
 
-const ButtonFillNone = () => {
+const ButtonFillNone = (props) => {
     return (
-        <TouchableOpacity style={styles.btnContainer}>
+        <TouchableOpacity style={styles.btnContainer} onPress={props.onPress}>
             <Text style={{ color: white, fontFamily: "LatoXb" }}>LOG IN</Text>
         </TouchableOpacity>
     );
@@ -17,8 +17,8 @@ const styles = StyleSheet.create({
         marginHorizontal: "5%",
         borderRadius: 5,
         height: 57,
-		borderWidth: 1,
-		borderColor: white,
+        borderWidth: 1,
+        borderColor: white,
         justifyContent: "center",
         alignItems: "center",
     },
