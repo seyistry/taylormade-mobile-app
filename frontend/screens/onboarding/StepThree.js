0817154b7f950a13ea27c86580d62dcf839f8c8a
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-12-13 12:32:00 +0100

    update

--- frontend/screens/onboarding/StepThree.js
+++ frontend/screens/onboarding/StepThree.js
@@ -1,14 +1,34 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { blackBg, white } from "../../utils/color";
 
 const StepThree = () => {
     return (
-        <View>
-            <Text>StepThree</Text>
+        <View style={styles.container}>
+            <View
+                style={{
+                    height: "20%",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <Text style={styles.text}>WHAT MOTIVATES YOU TO EXERCISE?</Text>
+            </View>
         </View>
     );
 };
 
 export default StepThree;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: blackBg,
+    },
+    text: {
+        color: white,
+        textAlign: "center",
+        fontFamily: "LatoXb",
+        fontSize: 20,
+    },
+});
