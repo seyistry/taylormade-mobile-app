1e040be47169c5a3378cb0e24daeda53c9e8411c
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-03-01 23:00:26 +0100

    update component to consume api

--- components/card/MotivateCard.js
+++ components/card/MotivateCard.js
@@ -1,7 +1,35 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { blackBg, blueBg, greyBg, white } from "../../utils/color";
-import { Octicons, Ionicons } from "@expo/vector-icons";
+import {
+    Octicons,
+    Ionicons,
+    MaterialIcons,
+    MaterialCommunityIcons,
+    Entypo,
+} from "@expo/vector-icons";
+
+const iconsDict = {
+    "BUILDING STRENGTH AND ENDURANCE": (
+        <MaterialCommunityIcons name="run-fast" size={24} color={white} />
+    ),
+    "IMPROVING HEALTH": (
+        <MaterialCommunityIcons
+            name="heart-plus-outline"
+            size={24}
+            color={white}
+        />
+    ),
+    "LOOKING GREAT": <Entypo name="man" size={24} color={white} />,
+    "IMMUNE SYSTEM SUPPORT": (
+        <MaterialCommunityIcons
+            name="head-plus-outline"
+            size={24}
+            color={white}
+        />
+    ),
+    "IMPROVE LIBIDO": <Ionicons name="male" size={30} color={white} />,
+};
 
 export default function MotivateCard(props) {
     return (
@@ -28,7 +56,7 @@ export default function MotivateCard(props) {
                 <Text style={styles.text}>{props.name}</Text>
             </View>
             <View style={{ width: "20%", alignItems: "center" }}>
-                <Ionicons name="male" size={30} color={white} />
+                {iconsDict[props.name]}
             </View>
         </TouchableOpacity>
     );
@@ -40,7 +68,7 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         height: "15%",
         flexDirection: "row",
-        marginBottom: 10,
+        marginBottom: 20,
         justifyContent: "center",
         alignItems: "center",
     },
