ace5efa3173d7962872ab2b5f6dc5644d217bb91
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-03-08 20:22:30 +0100

    add screens

--- navigation/PostAuthStack.js
+++ navigation/PostAuthStack.js
@@ -2,6 +2,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import SettingsMealsView from "../screens/appSettings/SettingsMealsView";
 import MealInfo from "../screens/meal/MealInfo";
+import ActiveExercise from "../screens/workout/ActiveExercise";
+import ExerciseDetails from "../screens/workout/ExerciseDetails";
 import Tabs from "./Tabs";
 
 const Stack = createStackNavigator();
@@ -16,6 +18,8 @@ function MyStack() {
             <Stack.Screen name="Home" component={Tabs} />
             <Stack.Screen name="Setting Meal" component={SettingsMealsView} />
             <Stack.Screen name="Meal Information" component={MealInfo} />
+            <Stack.Screen name="Exercise Details" component={ExerciseDetails} />
+            <Stack.Screen name="Active Exercise" component={ActiveExercise} />
         </Stack.Navigator>
     );
 }
