871a97f434cc9dc527339f2b4ff9a8c02cd58623
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-03-08 20:24:33 +0100

    add default active exercise view

---
+++ screens/workout/ActiveExercise.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function ActiveExercise() {
+  return (
+	<View>
+	  <Text>ActiveExercise</Text>
+	</View>
+  )
+}
+
+const styles = StyleSheet.create({})
